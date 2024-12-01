44118b43d6f742989fec8e7f10f26b04ccd2c93f
Adrien Blanchet <adrien.blanchet@live.fr>
2024-12-01 19:53:13 +0100

    first day

---
+++ 01/part1/code.C
@@ -0,0 +1,35 @@
+
+#include "GenericToolbox.Loops.h"
+
+#define DEBUG_VAR(var_) std::cout << "[DEBUG_VAR]: " << #var_ << " = " << var_ << std::endl;
+
+
+void code(){
+
+	auto lines = GenericToolbox::dumpFileAsVectorString("./input.txt");
+
+	std::vector<long> id1List; id1List.reserve(lines.size());
+	std::vector<long> id2List; id2List.reserve(lines.size());
+
+  for( auto& line : lines ){
+    auto elm = GenericToolbox::splitString(line, " ", true);
+
+    id1List.emplace_back(std::stol(elm[0]));
+    id2List.emplace_back(std::stol(elm[1]));
+  }
+
+  DEBUG_VAR(id1List.size());
+  DEBUG_VAR(id2List.size());
+
+  GenericToolbox::sortVector(id1List, [](long a_, long b_){ return a_ < b_; });
+  GenericToolbox::sortVector(id2List, [](long a_, long b_){ return a_ < b_; });
+
+  long sum{0};
+  for( auto [id1, id2] : GenericToolbox::Zip(id1List, id2List) ){
+    long dst = std::abs(id2 - id1);
+    sum += dst;
+  }
+
+  DEBUG_VAR(sum);
+
+}
---
+++ 01/part2/code.C
@@ -0,0 +1,33 @@
+
+#include "GenericToolbox.Loops.h"
+
+#define DEBUG_VAR(var_) std::cout << "[DEBUG_VAR]: " << #var_ << " = " << var_ << std::endl;
+
+
+void code(){
+
+	auto lines = GenericToolbox::dumpFileAsVectorString("./input.txt");
+
+	std::vector<long> id1List; id1List.reserve(lines.size());
+	std::vector<long> id2List; id2List.reserve(lines.size());
+
+  for( auto& line : lines ){
+    auto elm = GenericToolbox::splitString(line, " ", true);
+
+    id1List.emplace_back(std::stol(elm[0]));
+    id2List.emplace_back(std::stol(elm[1]));
+  }
+
+  DEBUG_VAR(id1List.size());
+  DEBUG_VAR(id2List.size());
+
+  long sum{0};
+  for( auto id1 : id1List ){
+    long nbAppears = std::count(id2List.begin(), id2List.end(), id1);
+    long score = nbAppears * id1;
+    sum += score;
+  }
+
+  DEBUG_VAR(sum);
+
+}
